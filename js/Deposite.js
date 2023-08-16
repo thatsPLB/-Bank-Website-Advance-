81cca2a6d013b5795c8b22b9d62d9bcb6686173a
Plabon Saha <sahaplabon66@gmail.com>
2023-08-16 21:25:39 +0600

    first commit

---
+++ js/Deposite.js
@@ -0,0 +1,6 @@
+// 1. add event listener to the deposite button
+// get deposite amount from the depopsite input field
+document.getElementById('btn-deposite').addEventListener('click', function(){
+    const depopsiteField = document.getElementById('deposit-field');
+    const newDepositeAmountstring = depopsiteField.value
+})
